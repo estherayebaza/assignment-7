38dd468eaa803eb026a6f8a43506fe352c1bee15
estherayebaza <79199523+estherayebaza@users.noreply.github.com>
2021-04-26 08:38:30 +0300

    Create assignment7.kt

--- src/assignment7.kt
+++ src/assignment7.kt
@@ -38,10 +38,10 @@ fun robot(age: Int) {
 }
 fun numberRanges() {
     for (i in 1..100) {
-      if (i % 3 == 0) {
+      if (i % 3 == 0&& i%5!==0) {
            println("Fizz")
         }
-        else if (i % 5 == 0) {
+        else if (i % 5 == 0 && i%3!==0) {
             println("Buzz")
         }
         else if (i % 3 == 0 && i % 5 == 0) {
